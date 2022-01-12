1869631ff53c54c392decca8a447ef46a77b60d5
augustojs1 <augustojsouza1@gmail.com>
2022-01-11 21:38:19 -0300

    feature: create drawer component

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align={"center"}>
-      <Box mr={"4"} textAlign={"right"}>
-        <Text>Augusto Souza</Text>
-        <Text color={"gray.300"} fontSize={"small"}>
-          augusto.souza@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr={"4"} textAlign={"right"}>
+          <Text>Augusto Souza</Text>
+          <Text color={"gray.300"} fontSize={"small"}>
+            augusto.souza@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size={"md"}
