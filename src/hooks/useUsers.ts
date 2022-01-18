9fd168982840d91724b54ba5ccb8f37426619b71
augustojs1 <augustojsouza1@gmail.com>
2022-01-17 21:40:51 -0300

    feat: create get users hook

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,40 @@
+import { useQuery } from "react-query";
+import { api } from "../services/api";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+};
+
+// jogando toda a lógica do react-query para um hook
+export async function getUsers(): Promise<User[]> {
+  const { data } = await api.get("users");
+
+  const users = data.users.map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
+    };
+  });
+
+  return users;
+}
+
+export function useUsers() {
+  return useQuery(
+    "users",
+    getUsers,
+    // tempo que o estado dos dados ficará como fresh (após isso ele será considerado stale e irá fazer uma nova requisição)
+    {
+      staleTime: 1000 * 5, // 5 seconds
+    }
+  );
+}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,34 +23,11 @@ import { Pagination } from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 import { useQuery } from "react-query";
 import { api } from "../../services/api";
+import { useUsers } from "../../hooks/useUsers";
 
 export default function UserList() {
   // recebe como parametro a chave para ser referenciada no localStorage e uma função de callback que vai retornar os dados
-  const { data, isLoading, isFetching, error } = useQuery(
-    "users",
-    async () => {
-      const { data } = await api.get("users");
-
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        };
-      });
-
-      return users;
-    },
-    // tempo que o estado dos dados ficará como fresh (após isso ele será considerado stale e irá fazer uma nova requisição)
-    {
-      staleTime: 1000 * 5, // 5 seconds
-    }
-  );
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
