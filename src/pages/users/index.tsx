23e861689c357edc3d2e60dc50b15a2d066b4239
augustojs1 <augustojsouza1@gmail.com>
2022-01-17 21:24:46 -0300

    feat: setting up axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,28 +22,35 @@ import Header from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 import { useQuery } from "react-query";
+import { api } from "../../services/api";
 
 export default function UserList() {
   // recebe como parametro a chave para ser referenciada no localStorage e uma função de callback que vai retornar os dados
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+  const { data, isLoading, isFetching, error } = useQuery(
+    "users",
+    async () => {
+      const { data } = await api.get("users");
 
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        };
+      });
 
-    return users;
-  });
+      return users;
+    },
+    // tempo que o estado dos dados ficará como fresh (após isso ele será considerado stale e irá fazer uma nova requisição)
+    {
+      staleTime: 1000 * 5, // 5 seconds
+    }
+  );
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -60,6 +67,9 @@ export default function UserList() {
           <Flex mb={"8"} justify={"space-between"} align={"center"}>
             <Heading size={"lg"} fontWeight={"normal"}>
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size={"sm"} color="gray.500" ml={"4"} />
+              )}
             </Heading>
             <Link href={"/users/create"} passHref>
               <Button
